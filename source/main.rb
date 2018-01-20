0955cea0028ba8e1a40bcebb6f16dfa2d93bd42c
nagami <nagamitakaaki@nagamitakaakinoMacBook-Air.local>
2018-01-20 13:04:30 +0900

    POSTメソッドのテストを実行

--- source/main.rb
+++ source/main.rb
@@ -3,7 +3,29 @@ require "bundler/setup"
 require 'sinatra'
 require "json"
 
+#postのテスト①
 get '/' do
-  "Hello world" #最初のサンプル
-  "日本語のテスト"
+  erb :input_form
+end
+
+#postのテスト②
+post '/login' do
+  login_name = params[:login]
+  password = params[:pass]
+
+  hash = {
+             login_info: "こんにちは、#{login_name}さん パスワードは#{password}"
+
+           }
+  return hash.to_json
+end
+
+get '/webhook' do
+
+  hash = {
+             speech: "こんにちは、こちらはサーバプログラムです。"
+
+           }
+  return hash.to_json
+
 end
