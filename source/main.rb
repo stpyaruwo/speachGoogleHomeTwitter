08170797c1326365d564e864406eeb098eb4bdc5
nagami <nagamitakaaki@nagamitakaakinoMacBook-Air.local>
2018-01-21 14:44:55 +0900

    値代入のテスト

--- source/main.rb
+++ source/main.rb
@@ -24,10 +24,22 @@ end
 
 post '/webhook' do
 
-  hash = {
-             speech: "こんにちは、こちらはサーバプログラムです。atsushiさん"
+  #JSONパラメータを読み込む
+  obj = JSON.parse(request.body.read)
+  keyword = obj["result"]["parameters"]["keyword"]
 
-           }
+  #keywordにオブジェクトが代入されてる場合
+  if keyword != nil
+      hash = {
+            speech: "#{keyword}とおっしゃいましたよね!"
+      }
+  else
+      hash = {
+              speech: "こんにちは、こちらはサーバプログラムです。atsushiさん"
+
+          }
+  end
+
   return hash.to_json
 
 end
