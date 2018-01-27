c1c6348a8a17389dd331282f70273a068b29d5f2
nagami <nagamitakaaki@nagamitakaakinoMacBook-Air.local>
2018-01-28 02:09:57 +0900

    TwitterReadrの修正(2)

--- source/TwitterReader.rb
+++ source/TwitterReader.rb
@@ -18,24 +18,27 @@ module TwitterRead
       def readtweet(tweets_num)
           #tweetsに数字が入っているかチェックする
 
-        #  if tweets_num != nil
+          if tweets_num != nil
               #ツイートを配列で受け取る
-        #      tweets = @client.home_timeline( { count: tweets_num} )
-        #      says = "はい！、#{tweets.count}件分のツイートを呼び込みます！"
+              tweets = @client.home_timeline( { count: tweets_num} )
+              says = "はい！、#{tweets.count}件分のツイートを呼び込みます！"
 
+              tweets.each_with_index do |tweet,count|
+                says = says + "#{count + 1} 件目は#{tweet.user.name}が、#{tweet.full_text}と呟いております。"
+              end
 
-        #      @postdialog = {
-        #            speech:says
-        #       }
+              @postdialog = {
+                    speech:says
+               }
 
-        #  else
-        #      @postdialog = {
-        #            speech:"すいません。失敗しました。"
-        #      }
-        #  end
-          @postdialog = {
-              speech: "認証OKです!"
-          }
+          else
+              @postdialog = {
+                    speech:"すいません。失敗しました。"
+              }
+          end
+          #@postdialog = {
+          #    speech: "認証OKです!"
+          #}
 
           return @postdialog
       end
