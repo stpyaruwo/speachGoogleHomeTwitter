2dacf3e500848a3d31094049e9ad85a5b8a1d1fd
nagami <nagamitakaaki@nagamitakaakinoMacBook-Air.local>
2018-02-12 23:02:31 +0900

    +呟き +トレンドの表示
    ＝＝
    
    以上の機能を追加

--- source/main.rb
+++ source/main.rb
@@ -14,13 +14,12 @@ class Main < Sinatra::Base
   include TwitterRead #TwitterReaderのモジュールをinclude
   include Webhook
 
-    #settings.変数名　で呼び出すことができる
+    #settings.変数名で呼び出すことができる
     configure do
       set :twitterread, TwitterReader.new
       set :postdialog, Hash.new
     end
 
-    #音声の使い方(how to use)
       get '/' do
         "Hello"
       end
@@ -28,42 +27,44 @@ class Main < Sinatra::Base
       #DialogFlowからのpost
       post '/webhook' do
         #①DialogFlowからjsonリクエストを受け取る
-               obj = JSON.parse(request.body.read)
-               #②リクエストの内容によって処理を変える
-               intent = obj["result"]["parameters"]["intent"]
+          begin
+            obj = JSON.parse(request.body.read)
+            #②リクエストの内容によって処理を変える
+            intent = obj["result"]["parameters"]["intent"]
 
-               case intent
-               when "callme" then
-                  settings.postdialog =  Callme.new().input("#{obj["result"]["parameters"]["keyword"]}")
+            case intent
+            when "callme" then
+               settings.postdialog =  Callme.new().input("#{obj["result"]["parameters"]["keyword"]}")
 
-               #webhookの動作確認
-               when "webhook" then
-                   settings.postdialog = My_webhook.new().input
+              #webhookの動作確認
+            when "webhook" then
+                settings.postdialog = My_webhook.new().input
 
-               when "tweetread" then
-                 #ツイートの取得件数s
-                     tweets_num = obj["result"]["parameters"]["number"]
-                 #ホームラインラインから、指定された数字まで、取得する
-                     settings.postdialog = Twitter_homeline.new().input(tweets_num)
-               when "twitter_tweet" then
-                 #呟く
-                     tweet = obj["result"]["parameters"]["tweet"]
-                     settings.postdialog = Twitter_tweet.new().input("#{tweet}")
-               else
-                   settings.postdialog = {
-                         speech: "全てにヒットしませんでした"
-                   }
-               end
-               #③DialogFlowに返す
-               return settings.postdialog.to_json
+            #ツイートの取得
+            when "tweetread" then
+              tweets = obj["result"]["parameters"]["number"]
+              settings.postdialog = Twitter_homeline.new().input(tweets)
+
+              #呟き
+            when "twitter_tweet" then
+              tweet = obj["result"]["parameters"]["tweet"]
+              settings.postdialog = Twitter_tweet.new().input("#{tweet}")
+
+            when "twitter_trend" then
+              number = obj["result"]["parameters"]["number"]
+              settings.postdialog = Twitter_trend.new().input("#{number}")
+            else
+                  settings.postdialog = {
+                      speech: "全てにヒットしませんでした"
+                    }
+            end
+          rescue => e
+            settings.postdialog = {
+                speech: "エラーが発生しました #{e.message} #{e.backtrace}"
+             }
+          end
+          #③DialogFlowに返す
+          return settings.postdialog.to_json
       end
-
-      #speech用のmethod String => Hash
-      def speechhome(says)
-          speechhash = {
-              speech:says
-          }
-          return speechhash
-       end
     run!
 end
--- source/webhook.rb
+++ source/webhook.rb
@@ -1,19 +1,33 @@
 require "twitter"
+require "oauth"
+require "json"
 #post dialogを受け取る親クラス
 #処理をintentの処置を起票する
 
 module Webhook
    class Superwebhook
-     #グーグルが話す内容について記述する
+
       def initialize()
           @speechtoken = { speech: "" }
           @client = Twitter::REST::Client.new do |config|
-              config.consumer_key        = 'p0oXKXPpcYLKUvTiKJ0gOznfi'
-              config.consumer_secret     = '80PePGdetsgofHQOO2bdyGk7TSvIvxoWNItDhg1Fe5ZXf1VcFU'
-              config.access_token        = '2504606953-T5yUJQnUcqsEYtvxWyUt2vZFZIrHSsob0JwEUyl'
+              config.consumer_key         = 'p0oXKXPpcYLKUvTiKJ0gOznfi'
+              config.consumer_secret      = '80PePGdetsgofHQOO2bdyGk7TSvIvxoWNItDhg1Fe5ZXf1VcFU'
+              config.access_token            = '2504606953-T5yUJQnUcqsEYtvxWyUt2vZFZIrHSsob0JwEUyl'
               config.access_token_secret = 'zxUKm4W5DiJHkLA5CdOdXS83789hOVUWxJtnqG9hCoaU3'
           end
+
+          consumer = OAuth::Consumer.new(
+            @client.consumer_key,
+            @client.consumer_secret,
+            site:'https://api.twitter.com/'
+          )
+          @endpoint = OAuth::AccessToken.new(consumer , @client.access_token , @client.access_token_secret)
+
       end
+
+      def input
+      end
+
      #mainに返すHashデータを定義する
       def speech_voice(says)
         @speechtoken[:speech] = says
@@ -23,20 +37,25 @@ module Webhook
       def trim_http(url)
           #url = url.gsub(/http([s]|):\/\/[\wW\/:%#\$&\?\(\)~\.=\+\-]+/, "")
           url = url.gsub(%r{https?://[\w_.!*\/')(-]+}, "")
-
           return url
       end
-      #override
-      #mainからのデータを入れる
-      def input
+
+      def max_tweet(is_tweets,max)
+            if is_tweets <= max &&  1 <= is_tweets
+              return true
+            else
+              return false
+            end
       end
+
+
    end
 
    #言った言葉を読み上げる
    class Callme < Superwebhook
       def input(says)
-         speech_voice(says + "とおっしゃいましたね!")
-         return @speechtoken
+          speech_voice(says + "とおっしゃいましたね!")
+          return @speechtoken
       end
    end
 
@@ -52,17 +71,19 @@ module Webhook
   #ホームラインから、ツイートの情報を読み込む
    class Twitter_homeline < Superwebhook
      #読み込むツイート数を決める
-      def input(tweets_num)
-         tweets_num = tweets_num.to_i
+      def input(tweets)
+         tweets = tweets.to_i rescue "error"
 
-        if tweets_num.kind_of?(Integer)
-             tweets = @client.home_timeline( { count: tweets_num} )
+        if tweets.kind_of?(Integer) && max_tweet(tweets, 30)
+             tweets = @client.home_timeline( { count: tweets} )
              says = "はい！、#{tweets.count}件分のツイートを呼び込みます！"
 
              tweets.each_with_index do |tweet,count|
-               says = says + "#{count + 1} 件目は#{tweet.user.name}が、#{trim_http(tweet.full_text)}と呟いております。"
+                says = says + "#{count + 1} 件目は#{tweet.user.name}が、#{trim_http(tweet.full_text)}と呟いております。"
              end
-
+             speech_voice(says)
+        elsif  max_tweet(tweets, 30)
+            says = "#{tweets}件は読み込めません"
             speech_voice(says)
         else
             speech_voice("すいません。失敗しました。")
@@ -70,7 +91,7 @@ module Webhook
         return @speechtoken
       end
    end
-   
+
   #呟く
    class Twitter_tweet < Superwebhook
       def input(says)
@@ -81,4 +102,35 @@ module Webhook
       end
     end
 
+    #日本のトレンドをあげる
+    class Twitter_trend < Superwebhook
+
+      def input(tweets)
+        tweets = tweets.to_i rescue "error"
+
+        if tweets.kind_of?(Integer) && max_tweet(tweets, 30)
+          response = @endpoint.get("https://api.twitter.com/1.1/trends/place.json?id=23424856")
+          result = JSON.parse(response.body)
+          says = "はい！#{tweets}件分のトレンドを読み込みます"
+              0.upto(tweets - 1) do |count|
+                  says =  says + "#{count + 1}件目は、 #{result[0]["trends"][count]["name"]}です"
+              end
+           speech_voice(says)
+
+        elsif tweets.kind_of?(Integer)
+            says = "#{tweets}件は読み込めません"
+            speech_voice(says)
+        else tweets == "error"
+           says = "エラーが発生しました。"
+           speech_voice(says)
+        end
+
+        return @speechtoken
+      end
+    end
+
+    #世界のトレンドの10位をあげる
+    class Twitter_world_trend < Superwebhook
+
+    end
 end
